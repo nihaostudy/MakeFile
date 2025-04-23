3deeb68ba5291be38560f2a650af29d54736e36f
nihaostudy <abc18379069297@163.com>
2025-04-23 11:20:33 +0800

    2025.04.23

---
+++ Makefile_test/2.编写Makefile动态链接库库/add.c
@@ -0,0 +1,6 @@
+
+#include "so_test.h"
+int add(int a , int b){
+    printf("运行到了add函数\n");
+    return a + b + 100;
+}   
---
+++ Makefile_test/2.编写Makefile动态链接库库/test.c
@@ -0,0 +1,7 @@
+#include "so_test.h"
+
+int main(){
+    add(1,2);
+    sub(3,1);
+    return 0;
+}
